2666b0d2ce9d058caa20d31f43ea61de7071b25a
innonka <innon@yandex.ru>
2023-02-18 16:50:30 +0200

    add js file

---
+++ script.js
@@ -0,0 +1,12 @@
+var workDay = [
+  { time: "9:00 am", event: "" },
+  { time: "10:00 am", event: "" },
+  { time: "11:00 am", event: "" },
+  { time: "12:00 pm", event: "" },
+  { time: "1:00 pm", event: "" },
+  { time: "2:00 pm", event: "" },
+  { time: "3:00 pm", event: "" },
+  { time: "4:00 pm", event: "" },
+  { time: "5:00 pm", event: "" }
+];
+
